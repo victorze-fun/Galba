07d563de3185272e72320b16d07a219da4e16890
Victor Hugo <victorzevallos@protonmail.com>
2019-02-17 23:28:17 -0500

    First commit

---
+++ src/main/java/pe/victorze/galba/GalbaApplication.java
@@ -0,0 +1,13 @@
+package pe.victorze.galba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GalbaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GalbaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/victorze/galba/GalbaApplicationTests.java
@@ -0,0 +1,16 @@
+package pe.victorze.galba;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GalbaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
